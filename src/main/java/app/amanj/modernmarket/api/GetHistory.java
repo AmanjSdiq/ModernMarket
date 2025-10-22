7e9978173f68823d97efe2e021c25700ccbd7e36
amanj.sdiq <amanj8it@gmail.com>
2025-10-22 22:41:34 +0300

    Add some API template

---
+++ src/main/java/app/amanj/modernmarket/api/GetHistory.java
@@ -0,0 +1,16 @@
+package app.amanj.modernmarket.api;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Log4j2
+@RestController
+@RequestMapping("")
+public class GetHistory {
+    @GetMapping("/getHistory")
+    public String getHistory() {
+        return "getHistory";
+    }
+}
