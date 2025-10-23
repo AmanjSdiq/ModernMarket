4a0f2f0746b92321c9088806ef262fbf140ac405
amanj.sdiq <amanj8it@gmail.com>
2025-10-23 12:27:57 +0300

    Add silver API with all unit details

--- src/main/java/app/amanj/modernmarket/api/GetPrice.java
+++ src/main/java/app/amanj/modernmarket/api/GetPrice.java
@@ -14,10 +14,12 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 @RequestMapping("get/price")
 public class GetPrice {
-    @GetMapping("/gold")
-    public String getPrice(@RequestParam(defaultValue = "oz") String unit) {
 
-        RestTemplate restTemplate = new RestTemplate();
+    private RestTemplate restTemplate = new RestTemplate();
+
+    @GetMapping("/gold")
+    public String gold(@RequestParam(defaultValue = "oz") String unit) {
+
         String url = "https://api.gold-api.com/price/XAU";
         String result = restTemplate.getForObject(url, String.class);
         log.info(result);
@@ -42,4 +44,33 @@ public class GetPrice {
             default -> "Invalid unit";
         };
     }
+
+    @GetMapping("/silver")
+    public String silver(@RequestParam(defaultValue = "oz") String unit) {
+
+        String url = "https://api.gold-api.com/price/XAG";
+        String result = restTemplate.getForObject(url, String.class);
+        log.info(result);
+        ResponseEntity<Price> exchange = restTemplate.exchange(url, HttpMethod.GET, null, Price.class);
+        log.info(exchange.getBody());
+
+        double pricePerOunce = exchange.getBody().getPrice();
+
+        double ouncesPerKg = 32.1507;
+
+        double priceKg = pricePerOunce * ouncesPerKg;
+
+        double priceGram = priceKg / 1000;
+
+        double priceMithqal = priceGram * 4.25;
+
+        return switch (unit) {
+            case "oz" -> String.valueOf(pricePerOunce);
+            case "kg" -> String.valueOf(priceKg);
+            case "g" -> String.valueOf(priceGram);
+            case "mithqal" -> String.valueOf(priceMithqal);
+            default -> "Invalid unit";
+        };
+    }
+
 }
